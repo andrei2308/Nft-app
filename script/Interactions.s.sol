f399b2f47db480c606ec4928804c045f9c05cb4f
Andrei <chitoiu.andrei2@yahoo.com>
2024-12-27 16:01:41 +0200

    fix: import MoodNft in interactions and add function run visibility

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
+import {MoodNft} from "../src/MoodNft.sol";
 
 contract MintBasicNft is Script {
     function run() external {
@@ -35,7 +36,7 @@ contract MintMoodNft is Script {
 }
 
 contract FlipMood is Script {
-    function run() {
+    function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
         flipMoodOnContract(mostRecentlyDeployed);
     }
